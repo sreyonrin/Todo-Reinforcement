57d74f3a5c6a3b92b8004dd1a0b94965161b5afb
Sreyorn <rinsreyorn9@gmail.com>
2020-02-13 20:52:17 +0700

    chellenge-02

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
-const DATE_HTML = document.querySelector('#date');
-const LIST_HTML = document.querySelector('#list');
-const INPUT_HTML = document.querySelector('#input');
+const DATE_HTML = document.querySelector("#date");
+const LIST_HTML = document.querySelector("#list");
+const INPUT_HTML = document.querySelector("#input");
 
 const CHECK_STYLE = "fa-check-circle";
 const UNCHECK_STYLE = "fa-circle-thin";
 const LINE_THOUGH_STYLE = "lineThrough";
 
 const options = {
-// TODO
   weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric"
 };
+const today = new Date();
+DATE_HTML.innerHTML = today.toLocaleString("en-US", options);
 
-const TODAY = new Date();
-DATE_HTML.innerHTML = TODAY.toLocaleString("en-US",options)
-// TODO: to displa the date in HTML
+document.addEventListener("keyup", event => {
+	if(event.code){
+		const intputValue = INPUT_HTML.value;
+		if(intputValue != ""){
+			//1 add todo
+			console.log(intputValue);
+			//2 clear input
+			clearInput();
+		}
+		
+	}
+	
+	// TODO: get Enter code
 
+	// TODO: get value from input 
+	// TODO: check condition if not null and console value
+	// TODO: clear  input 
+});
+
+function clearInput() {
+	INPUT_HTML.value = "";	
+}
