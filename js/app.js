8d9ddb1935d1a5c0e82c1f1f8e2e250928eaca5e
Sreyorn <rinsreyorn9@gmail.com>
2020-02-14 20:48:54 +0700

    code chellenge 10

--- js/app.js
+++ js/app.js
@@ -17,7 +17,17 @@ DATE_HTML.innerHTML = today.toLocaleString("en-US", options);
 
 let todoItems = [];
 let id = 0;
+let data = localStorage.getItem("TODO");
+if (data) {
+  // 1. Update the list of items
+  todoItems = JSON.parse(data);
 
+  // 2. Update the current id
+  id = todoItems.length;
+
+  // 3. refresh the view
+  updateList();
+}
 
 document.addEventListener("keyup", event => {
 	if (event.code === "Enter") {
@@ -35,31 +45,40 @@ document.addEventListener("keyup", event => {
 function clearInput() {
 	INPUT_HTML.value = "";
 }
+// TODO: get id and return value
+function getTodoFromId(id) {
+  for(let item of todoItems){
+    if(item.id === id){
+      return item;
+    }
+  }
+  return null;
+}
 
 function addTodo(todoName) {
+  //1. push value to array
   todoItems.push({name: todoName, id : id, done: false});
-  // 2 Increment id
+  //2. Increment id
   id++;
   //3. save JSON
-  saveJson()
-  //4.update html
+  saveJson() 
+  // 4. update html
 	updateList();
 }
 
 function updateList() {
   let code = "";
   for(let item of todoItems) {
-	// TODO
-    code += `<li class = "item">
-        <i class = "fa ${item.done ? CHECK_STYLE : UNCHECK_STYLE} co" job = "complete" id = "${item.id}"></i>
-        <p class = "text ${item.done ? LINE_THOUGH_STYLE: ""}">${item.name}</p>
-        <i class = "fa fa-trash-o de" job = "delete" id = "${item.id}"></i>
+    code += `<li class="item">
+        <i class="fa ${item.done ? CHECK_STYLE : UNCHECK_STYLE} co" job="complete" id="${item.id}"></i>
+        <p class="text ${item.done ? LINE_THOUGH_STYLE: "" }">${item.name}</p>
+        <i class="fa fa-trash-o de" job="delete" id="${item.id}"></i>
     </li>`;
-  // TODO: display to 
   LIST_HTML.innerHTML = code;
+  
   }
 }
 
-function saveJson(){
- localStorage.setItem("TODO",JSON.stringify(todoItems));
+function saveJson() {
+   localStorage.setItem("TODO", JSON.stringify(todoItems));
 }
